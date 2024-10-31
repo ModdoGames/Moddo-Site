bcf0507c40653a9f3956e93b9bf6ef6512975a52
Moddo Games <149247984+ModdoGames@users.noreply.github.com>
2024-10-31 08:38:11 +0000

    Delete scrollDir.js

--- scrollDir.js
+++
@@ -1,7 +0,0 @@
-document.onscroll = () => {
-  // adjust for the difference between window height and width
-  let percentScrolled = window.pageYOffset / window.innerHeight;
-  let toScroll = percentScrolled * window.innerWidth;
-  // scroll horizontally
-  document.getElementById("scroll").style.left = "-" + toScroll + "px";
-}
